4564764eb0809c95d623890075402d2e54618f75
Chisom <iwowochisom@gmail.com>
2019-10-14 20:08:01 +0100

    Add app.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'app.js',
   },
   module: {
     rules: [
